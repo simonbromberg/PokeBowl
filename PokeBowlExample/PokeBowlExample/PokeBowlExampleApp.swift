e036d5d0b05383ec44ca7dbd4aaaa38ef0ac2ef8
Simon Bromberg <bshimmy@gmail.com>
2023-11-28 12:35:50 -0500

    PokeBowlExample project

---
+++ PokeBowlExample/PokeBowlExample/PokeBowlExampleApp.swift
@@ -0,0 +1,18 @@
+//
+//  PokeBowlExampleApp.swift
+//  PokeBowlExample
+//
+//  Created by Simon Bromberg on 2023-11-28.
+//
+
+import PokeBowl
+import SwiftUI
+
+@main
+struct PokeBowlExampleApp: App {
+  var body: some Scene {
+    WindowGroup {
+      PokemonView()
+    }
+  }
+}
