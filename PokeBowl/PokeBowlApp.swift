404137da6bd54401835af31bf962fe9b0e1e8daa
Simon Bromberg <bshimmy@gmail.com>
2023-11-28 01:01:25 -0500

    Initial Commit

---
+++ PokeBowl/PokeBowlApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokeBowlApp.swift
+//  PokeBowl
+//
+//  Created by Simon Bromberg on 2023-11-28.
+//
+
+import SwiftUI
+
+@main
+struct PokeBowlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
